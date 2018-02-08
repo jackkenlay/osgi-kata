3bfcd2f3df2dc980b458fe0d2a3b586f06cf9075
Jack Kenlay <jackk@ensemble.com>
2018-02-08 11:27:02 +0000

    Stage 1

---
+++ stage-1/my-osgi-app/src/main/java/com/ensemble/MyOSGiApp.java
@@ -0,0 +1,18 @@
+package com.ensemble;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+
+@Component(service = MyOSGiApp.class, immediate = true)
+public class MyOSGiApp {
+    @Activate
+    public void activate() {
+        System.out.println("My OSGi MyOSGiApp: Component Activated");
+    }
+
+    @Deactivate
+    public void deactivate() {
+        System.out.println("My OSGi MyOSGiApp: Component Deactivated");
+    }
+}
